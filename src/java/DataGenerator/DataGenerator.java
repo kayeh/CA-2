325feb12b999cb45f4aab261f3820f098854fae1
cph-js226 <cph-js226@cphbusiness>
2016-03-16 11:30:18 +0100

    phone generator added

--- src/java/DataGenerator/DataGenerator.java
+++ src/java/DataGenerator/DataGenerator.java
@@ -5,7 +5,8 @@ import java.util.List;
 import java.util.Random;
 
 public class DataGenerator {
-
+    
+    // Prefix & Affix to generate the random Strings
     static String[] lastnames = {"hansen", "jonsen", "olsen", "jonsen", "carlsen", "larsen", "simonsen", "ditlevsen", "graabek", "bohn", "szigethy", "wolff", "morgentailer", "kjærsgaard", "Mayweather", "Boldsen", "McGregor", "Andreasen", "Larsson"};
     static String[] maleNames = {"Alexander", "Alex", "andreas", "adrian", "axel", "august", "ali", "ahmed", "Bo", "bent", "carl", "emil", "eli", "eske", "fin", "finn", "frederik", "gert", "gunner", "Henrik", "henning", "hans", "hans-henrik", "Ingolf", "Jonathan", "Jonas", "Junes", "Jan", "Jarl"};
     static String[] femaleNames = {"Andrea", "Alexandra", "Alex", "Birgitte", "Berit", "britt", "bea", "bente", "Carla", "Caroline", "Caio", "Emilie", "Emilia", "Eli", "Fiola", "Gerda", "gundbrit", "Heidi", "Ida", "Ida-Marie", "Josefine", "Julie", "karoline", "Klaudia", "Leila", "Maj-britt"};
@@ -13,20 +14,34 @@ public class DataGenerator {
     static String[] streetMid = {"fiol", "rose", "kælk", "sten", "skovl", "bjørn", "sol", "ræv", "fasan", "mose", "bule"};
     static String[] streetEnd = {"s Alle", "vej", "s Stræde", "gade", "s Torv"};
     static String[] estateType = {"hus", "tv.", "th."};
-
+    static String[] phoneDescription = {"Hjemme", "Mobil", "Skype", "Arbejde"};
+    
     //Variables for person
     private static int gender;
     private static Random random = new Random();
     private static String forename;
     private static String lastname;
-    private static String description;
-    //    private static List<String> persons = new ArrayList<>();
-//    private static List<String> streets = new ArrayList<>();
+    private static String description; 
+//  private static List<String> persons = new ArrayList<>();
+//  private static List<String> streets = new ArrayList<>();
 
     //Variables for street
     private static int randomIndex;
     private static int randomLastname;
     private static String streetName;
+    
+
+    //Variables for Phone
+    private static String number;
+    
+    
+    public static String makePhone(){
+        randomIndex = random.nextInt(99999998)+1;
+        number = Integer.toString(randomIndex);
+        randomIndex = random.nextInt(phoneDescription.length);
+        number += "," +phoneDescription[randomIndex];
+        return number;
+    }
 
     public static String makeAddInfo() {
         randomIndex = random.nextInt(5)+1;
@@ -78,6 +93,7 @@ public class DataGenerator {
 //            persons.add("INSERT INTO PERSON VALUES (null,'" + forename + "'" + "," + "'" + lastname + "'" + ");");
 //        }
         return forename + "," + lastname;
+        
 
     }
 
