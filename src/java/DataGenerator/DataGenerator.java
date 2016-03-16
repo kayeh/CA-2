324ac5ce2d5599f2ed837e6dbe29497135ab3992
cph-js226 <cph-js226@cphbusiness>
2016-03-16 11:03:36 +0100

    2. DataGenerator with a method to add streets with additional information alle street names is in danish-language

--- src/java/DataGenerator/DataGenerator.java
+++ src/java/DataGenerator/DataGenerator.java
@@ -9,24 +9,48 @@ public class DataGenerator {
     static String[] lastnames = {"hansen", "jonsen", "olsen", "jonsen", "carlsen", "larsen", "simonsen", "ditlevsen", "graabek", "bohn", "szigethy", "wolff", "morgentailer", "kjærsgaard", "Mayweather", "Boldsen", "McGregor", "Andreasen", "Larsson"};
     static String[] maleNames = {"Alexander", "Alex", "andreas", "adrian", "axel", "august", "ali", "ahmed", "Bo", "bent", "carl", "emil", "eli", "eske", "fin", "finn", "frederik", "gert", "gunner", "Henrik", "henning", "hans", "hans-henrik", "Ingolf", "Jonathan", "Jonas", "Junes", "Jan", "Jarl"};
     static String[] femaleNames = {"Andrea", "Alexandra", "Alex", "Birgitte", "Berit", "britt", "bea", "bente", "Carla", "Caroline", "Caio", "Emilie", "Emilia", "Eli", "Fiola", "Gerda", "gundbrit", "Heidi", "Ida", "Ida-Marie", "Josefine", "Julie", "karoline", "Klaudia", "Leila", "Maj-britt"};
-    static String[] vejBegyndelse = {"Sorte", "Hvide", "Grøne,", "Orange", "Gule"};
-    static String[] vejMidt = {"Fiol", "Rose", "Kælk", "Sten", "Skovl", "Bjørn", "Sol", "Ræv", "Fasan", "Mose", "Bule"};
-    static String[] vejEndelse = {" Alle", "Vej", "Stræde", "Gade", "Torv"};
+    static String[] streetBegin = {"Sorte", "Hvide", "Grøne", "Orange", "Gule"};
+    static String[] streetMid = {"fiol", "rose", "kælk", "sten", "skovl", "bjørn", "sol", "ræv", "fasan", "mose", "bule"};
+    static String[] streetEnd = {"s Alle", "vej", "s Stræde", "gade", "s Torv"};
+    static String[] estateType = {"hus", "tv.", "th."};
 
-    static int malesSize = maleNames.length;
-    int femaleSize = femaleNames.length;
-    static int gender;
-    static Random random = new Random();
-    static String forename;
-    static String lastname;
-    static List<String> persons = new ArrayList<>();
-    static List<String> streets = new ArrayList<>();
+    //Variables for person
+    private static int gender;
+    private static Random random = new Random();
+    private static String forename;
+    private static String lastname;
+    private static String description;
+    //    private static List<String> persons = new ArrayList<>();
+//    private static List<String> streets = new ArrayList<>();
 
-    public static int randomIndex;
-    public static int randomLastname;
+    //Variables for street
+    private static int randomIndex;
+    private static int randomLastname;
+    private static String streetName;
 
-    public static void makeStreet() {
+    public static String makeAddInfo() {
+        randomIndex = random.nextInt(5)+1;
+        description = Integer.toString(randomIndex) + ". ";
+        randomIndex = random.nextInt(estateType.length);
+        if (randomIndex == 0) {
+            return estateType[0];
+        } else {
+            description += estateType[randomIndex];
+            return description;
+        }
 
+    }
+
+    public static String makeStreet() {
+        randomIndex = random.nextInt(streetBegin.length);
+        streetName = streetBegin[randomIndex];
+        randomIndex = random.nextInt(streetMid.length);
+        streetName += streetMid[randomIndex];
+        randomIndex = random.nextInt(streetEnd.length);
+        streetName += streetEnd[randomIndex];
+        randomIndex = random.nextInt(250);
+
+        return streetName + " " + Integer.toString(randomIndex);
     }
 
     public static String makePerson() {
@@ -35,12 +59,12 @@ public class DataGenerator {
         gender = random.nextInt(2);
         randomLastname = random.nextInt(lastnames.length);
 
-        //Random Male Forename
+        //Random Male Forename (with capital first-letter)
         if (gender == 0) {
-            randomIndex = random.nextInt(malesSize);
+            randomIndex = random.nextInt(maleNames.length);
             forename = maleNames[randomIndex].substring(0, 1).toUpperCase() + maleNames[randomIndex].substring(1, maleNames[randomIndex].length());
         }
-        //Random Female Forename
+        //Random Female Forename (with capital first-letter)
         if (gender == 1) {
             randomIndex = random.nextInt(femaleNames.length);
             forename = femaleNames[randomIndex].substring(0, 1).toUpperCase() + femaleNames[randomIndex].substring(1, femaleNames[randomIndex].length());
@@ -50,11 +74,10 @@ public class DataGenerator {
         lastname = lastnames[randomLastname].substring(0, 1).toUpperCase() + lastnames[randomLastname].substring(1, lastnames[randomLastname].length());
 
         //put into a list, and then print it    
-        if (persons.indexOf(forename) == -1 && persons.indexOf(lastname) == -1) {
-            persons.add("INSERT INTO PERSON VALUES (null,'" + forename + "'" + "," + "'" + lastname + "'" + ");");
-        }
-
-        return forename +","+ lastname;
+//        if (persons.indexOf(forename) == -1 && persons.indexOf(lastname) == -1) {
+//            persons.add("INSERT INTO PERSON VALUES (null,'" + forename + "'" + "," + "'" + lastname + "'" + ");");
+//        }
+        return forename + "," + lastname;
 
     }
 
